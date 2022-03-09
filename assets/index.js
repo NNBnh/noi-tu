bc96176967068ba5262a38930b571ab8cd0b2174
NNB <nnbnh@protonmail.com>
2022-03-09 10:27:16 +0000

    playable

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,13 @@
+const currentWord = document.getElementById("currentWord");
 const joinWord = document.getElementById("joinWord");
 
 joinWord.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    document.getElementById("currentWord").value = joinWord.value;
-    joinWord.value = null;
+
+    if (words.includes(currentWord.value + " " + joinWord.value)) {
+      currentWord.value = joinWord.value;
+      joinWord.value = null;
+    }
   }
 });
