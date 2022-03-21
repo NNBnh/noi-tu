da41a8ec0fc9b0eb81acb6b4b774e779fc82185a
NNB <nnbnh@protonmail.com>
2022-03-21 10:08:45 +0000

    modified:   client/main.js 	modified:   client/style.css

--- client/main.js
+++ client/main.js
@@ -47,6 +47,7 @@ function checkMode(event) {
   }
 }
 
+
 function startGameSingle() {
   gameMode = 1;
 
@@ -65,24 +66,6 @@ function startGameSingle() {
 
   addPiece();
   startTimer();
-}
-
-function endGame() { // TODO
-  document.title = "Nối Từ 100";
-
-  let usedWordList = "";
-
-  for (let i = 0; i < history.length; i++) {
-    usedWordList += (i + 1).toString() + ".&#9;" + history[i] + "\n";
-  }
-
-  pieces.innerHTML = `
-    <div id="usedWordList">
-      <pre>${usedWordList}</pre>
-    </div>
-    <div id="endGameNav">
-    </div>
-  `;
 }
 
 function startTimer() {
@@ -124,10 +107,10 @@ function checkWord(event) {
 
   if (gameMode === 1) {
     switch(joinWordFormated) {
-      case "<":
+      case "[":
         startGameSingle();
         break;
-      case ">":
+      case "]":
         endGame();
         break;
     }
@@ -157,6 +140,49 @@ function checkWord(event) {
   score += timeBonus;
   scoreDisplay.innerHTML = score;
   startTimer();
+}
+
+
+function endGame() {
+  document.title = "Nối Từ 100";
+
+  let usedWordList = "";
+
+  for (let i = 0; i < history.length; i++) {
+    usedWordList += (i + 1).toString() + ".&#9;" + history[i] + "\n";
+  }
+
+  pieces.innerHTML = `
+    <div id="usedWordList">
+      <pre>${usedWordList}</pre>
+      <div id="endGameNav">
+        <button onclick="menu()">
+          <img id="backButton" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0yMCAuNzU1bC0xNC4zNzQgMTEuMjQ1IDE0LjM3NCAxMS4yMTktLjYxOS43ODEtMTUuMzgxLTEyIDE1LjM5MS0xMiAuNjA5Ljc1NXoiLz48L3N2Zz4=" />
+        </button>
+        <input id="playerName" placeholder="Nhập tên" autocomplete="off" onkeyup="submitScore(event)" />
+        <button onclick="startGameSingle()">
+          <img id="replayButton" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik03IDloLTd2LTdoMXY1LjJjMS44NTMtNC4yMzcgNi4wODMtNy4yIDExLTcuMiA2LjYyMyAwIDEyIDUuMzc3IDEyIDEycy01LjM3NyAxMi0xMiAxMmMtNi4yODYgMC0xMS40NS00Ljg0NC0xMS45NTktMTFoMS4wMDRjLjUwNiA1LjYwMyA1LjIyMSAxMCAxMC45NTUgMTAgNi4wNzEgMCAxMS00LjkyOSAxMS0xMXMtNC45MjktMTEtMTEtMTFjLTQuNjYgMC04LjY0NyAyLjkwNC0xMC4yNDkgN2g1LjI0OXYxeiIvPjwvc3ZnPg==" />
+        </button>
+      </div>
+    </div>
+  `;
+}
+
+function submitScore(event) {
+  if (event.keyCode !== 13) return;
+
+  event.preventDefault();
+
+  const playerName = document.getElementById("playerName").value.trim();
+  const counter = history.length;
+  const usedWordList = history.join(", ");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("post", `https://docs.google.com/forms/d/e/1FAIpQLSem_C7O7GzgXNs8sWDSjn6s9J8br_objp5IUor7--j80XZiIQ/formResponse?usp=pp_url&entry.1011382549=${playerName}&entry.356858322=${counter}&entry.834877191=${score}&entry.595300868=${usedWordList}&submit=Submit`, true);
+  xhr.send();
+  window.open("https://docs.google.com/spreadsheets/d/16WMbIcs7HJ9fBC7qKPmj6wyNjImy51PZH0CzreuBQmk/edit?usp=sharing", "_blank").focus();
+
+  document.getElementById("playerName").disabled = true;
 }
 
 
